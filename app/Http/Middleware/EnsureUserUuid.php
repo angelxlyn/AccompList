771357edddce446e40029ec212732003b001f36f
Angel Lyn <angelvlynx@gmail.com>
2026-05-16 20:46:35 +0800

    fix: resolve 500 error in incognito

--- app/Http/Middleware/EnsureUserUuid.php
+++ app/Http/Middleware/EnsureUserUuid.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Cookie;
 use Symfony\Component\HttpFoundation\Response;
 
 class EnsureUserUuid
